f28d2c66864c8166ace82baebd39c5f807ccbba1
NastassiyaKh <nholodok@yandex.ru>
2023-02-10 18:11:07 +0300

    add User class

---
+++ src/main/java/by/itacademy/anastasiaholodok/javabasics/oop/User.java
@@ -0,0 +1,13 @@
+package by.itacademy.anastasiaholodok.javabasics.oop;
+
+public class User {
+    protected String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
