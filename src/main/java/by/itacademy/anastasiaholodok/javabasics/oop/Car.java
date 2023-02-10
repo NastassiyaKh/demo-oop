8cd07a722c67138b1fb7a73fe31881964ab4a6ae
NastassiyaKh <nholodok@yandex.ru>
2023-02-10 18:10:10 +0300

    add Car class

---
+++ src/main/java/by/itacademy/anastasiaholodok/javabasics/oop/Car.java
@@ -0,0 +1,67 @@
+package by.itacademy.anastasiaholodok.javabasics.oop;
+
+public class Car {
+    private int id;
+    private String mark;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
+    private int registrationNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(int registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+}
